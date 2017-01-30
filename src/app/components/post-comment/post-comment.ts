bd4c5fa458bb7aca53fd93149870407126e12f51
Alexandre <alexandre.lim@hotmail.com>
2017-01-30 18:08:56 +0100

    Comment create + get comment

--- src/app/components/post-comment/post-comment.ts
+++ src/app/components/post-comment/post-comment.ts
@@ -1,9 +1,12 @@
 import { Component, Input } from '@angular/core';
+import { PostService } from '../../services/index';
 import { Comment } from 'models';
+import { Post } from '../../models';
 
 @Component({
     templateUrl: 'post-comment.html',
     selector: 'post-comment'
 })
 export class PostCommentComponent{
+    @Input() comment: Post;
 }
