0555b2849af7247b5b4569531c300447224bca5f
Alexandre <alexandre.lim@hotmail.com>
2017-01-30 15:06:58 +0100

    Display format date + dynamic create channel

--- src/app/components/social-app/social-app.ts
+++ src/app/components/social-app/social-app.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, EventEmitter } from '@angular/core';
 import { Channel } from 'models';
 import { ChannelService } from 'services';
+import {PostSocketService} from 'services'
 import { ActivatedRoute } from '@angular/router';
 import { Router } from '@angular/router';
 
@@ -14,7 +15,8 @@ export class SocialAppComponent implements OnInit {
     constructor(
         private channelService: ChannelService,
         private route: ActivatedRoute,
-        private router : Router
+        private router : Router,
+        private postSocketService : PostSocketService
     ) {
     }
 
@@ -22,5 +24,12 @@ export class SocialAppComponent implements OnInit {
         this.channels = await this.channelService.getAll();
         // Redirect toward the first channel
         if (this.channels[0] != undefined) this.router.navigate(['channel/' + this.channels[0].id]);
+        //Listening if channel is create
+        this.postSocketService.onNewChannel((channel) => {
+            if( channel!= null){
+                console.log("Un nouveau channel a été crée ! ");
+                this.channels.push(channel);
+            }
+        });
     }
 }
